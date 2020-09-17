1d19c2008e9075004058be3fcdaf0b6ddbbfd791
Siddhey Sankhe <siddhey.sankhe@ninjacart.com>
2020-09-17 19:43:04 +0530

    intial commit

---
+++ src/main/java/com/example/UserWallet/UserWalletApplication.java
@@ -0,0 +1,13 @@
+package com.example.UserWallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserWalletApplication.class, args);
+	}
+
+}
