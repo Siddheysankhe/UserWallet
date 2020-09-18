b61f3d57b3f07fd4e2364d4fd3176ac200738c19
Siddhey Sankhe <siddhey.sankhe@ninjacart.com>
2020-09-18 14:53:34 +0530

    added controllers, services and repos (basic setup donw)

---
+++ src/main/java/com/example/UserWallet/controller/UserAccountController.java
@@ -0,0 +1,4 @@
+package com.example.UserWallet.controller;
+
+public class UserAccountController {
+}
---
+++ src/main/java/com/example/UserWallet/repository/UserAccountRepository.java
@@ -0,0 +1,4 @@
+package com.example.UserWallet.repository;
+
+public class UserAccountRepository {
+}
